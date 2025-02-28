c316abc234abaa88d71eac0c444d50e3385d1a8b
AJ Matthews <alistair.matthews@civica.co.uk>
2025-02-28 16:06:08 +0000

    Solution upgraded to .NET Aspire 9.1.0.

--- SupportTicketApi/SupportTicketApi.AppHost/Program.cs
+++ SupportTicketApi/SupportTicketApi.AppHost/Program.cs
@@ -7,4 +7,7 @@ var sql = builder.AddSqlServer("sql", port: 14329)
 builder.AddProject<Projects.SupportTicketApi_Api>("api")
     .WithReference(sql);
 
+builder.AddProject<Projects.SupportTicketApi_MigrationService>("migrations")
+    .WithReference(sql);
+
 builder.Build().Run();
