85b81d545284532a67452bc5252324bea4c07fd2
Alistair Matthews <alistair.matthews@civica.co.uk>
2025-03-07 16:34:24 +0000

    Upgraded solution to .NET Aspire 9.1.0 (#8)
    * Upgraded solution to .NET Aspire 9.1.0
    
    * Corrected the version of .NET.
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- SupportTicketApi/SupportTicketApi.AppHost/Program.cs
+++ SupportTicketApi/SupportTicketApi.AppHost/Program.cs
@@ -1,12 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sql = builder.AddSqlServer("sql")
+var sql = builder.AddSqlServer("sql", port: 14329)
+                 .WithEndpoint(name: "sqlEndpoint", targetPort: 14330)
                  .AddDatabase("sqldata");
 
 builder.AddProject<Projects.SupportTicketApi_Api>("api")
-    .WithReference(sql);
+    .WithReference(sql)
+    .WaitFor(sql);
 
 builder.AddProject<Projects.SupportTicketApi_MigrationService>("migrations")
-    .WithReference(sql);
+    .WithReference(sql)
+    .WaitFor(sql);
 
 builder.Build().Run();
