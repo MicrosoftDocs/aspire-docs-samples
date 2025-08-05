f123069ac67a39fa0fe392a27638a1b13e478d66
AJ Matthews <alistair.matthews@civica.co.uk>
2025-08-05 16:10:37 +0100

    Lab steps completed.

--- SupportTicketApi/SupportTicketApi.AppHost/Program.cs
+++ SupportTicketApi/SupportTicketApi.AppHost/Program.cs
@@ -4,8 +4,13 @@ var sql = builder.AddSqlServer("sql", port: 14329)
                  .WithEndpoint(name: "sqlEndpoint", targetPort: 14330)
                  .AddDatabase("sqldata");
 
-builder.AddProject<Projects.SupportTicketApi_Api>("api")
+var migrations = builder.AddProject<Projects.SupportTicketApi_MigrationService>("migrations")
     .WithReference(sql)
     .WaitFor(sql);
+
+builder.AddProject<Projects.SupportTicketApi_Api>("api")
+    .WithReference(sql)
+    .WithReference(migrations)
+    .WaitForCompletion(migrations);
 
 builder.Build().Run();
