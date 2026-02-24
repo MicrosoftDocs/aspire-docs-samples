2aed38ebb75489fb94c1cd9c7884629b81595dec
David Pine <david.pine@microsoft.com>
2026-02-24 08:29:10 -0600

    Add Completed property and AddCompleted migration for tutorial solution state

---
+++ SupportTicketApi/SupportTicketApi.MigrationService/Migrations/20260224142857_AddCompleted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupportTicketApi.MigrationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Tickets");
+        }
+    }
+}
